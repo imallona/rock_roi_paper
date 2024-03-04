f5f8150a2e05f810505db2affef41bb55474e1d2
Giulia Moro2 (gimoro) <gimoro@MLS-KBGRP-M02.local>
2024-03-04 17:44:36 +0100

    counting with featurecounts with new gtf

---
+++ 04_leukemia/test_mapping/bulk/bulk_test_small_genome_featurecounts_sim.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+##
+
+WD=/home/gmoro/simulated_kiel_mapping/
+GENOME=/home/gmoro/indices
+EXPERIMENT=bulk_small_genome_standard
+FEATURECOUNTS=/home/imallona/soft/subread-2.0.4-source/bin/featureCounts
+
+export PATH=/home/imallona/soft/star/STAR-2.7.10b/source:$PATH
+
+NTHREADS=10
+nice -n 19 STAR --runThreadN $NTHREADS \
+     --genomeDir $GENOME/bcr_abl_downsampled \
+     --readFilesCommand zcat \
+     --outFileNamePrefix $EXPERIMENT \
+     --readFilesIn $WD/pos_neg_r2.fastq.gz \
+     --outSAMattributes All \
+     --sjdbOverhang 61 \
+     --outSAMtype BAM SortedByCoordinate \
+     --sjdbGTFfile $GENOME/bcr_abl_downsampled.gtf \
+     --outFilterMismatchNmax 10 \
+     --seedSearchStartLmax 50 \
+     --outFilterMatchNmin 0
+
+# to get the information on the CIGAR
+
+samtools view $EXPERIMENT"Aligned.sortedByCoord.out.bam" | cut -f 1,3,6 > $EXPERIMENT"_CIGAR.txt"
+
+# counting with custom gtf
+
+$FEATURECOUNTS -a ~/kiel_leukemia_data/captured_bcr_abl.gtf \
+                -o "$item" \
+                $EXPERIMENT"Aligned.sortedByCoord.out.bam" \
+                -F GTF \
+                -t exon \
+                -g gene_id \
+                -f \
+                -O \
+                -M  \
+                -T "$NTHREADS" \
+                --byReadGroup
