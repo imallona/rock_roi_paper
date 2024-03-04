b3ade541424249acff9a89064a2b5c62e15c31b2
Giulia Moro2 (gimoro) <gimoro@MLS-KBGRP-M02.local>
2024-03-04 13:34:59 +0100

    test with simulated reads to check for off-targets

---
+++ 04_leukemia/test_mapping/bulk/bulk_test_full_genome.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+##
+
+WD=/home/gmoro/simulated_kiel_mapping/
+GENOME=/home/gmoro/indices
+EXPERIMENT=bulk_small_genome_standard
+
+export PATH=/home/imallona/soft/star/STAR-2.7.10b/source:$PATH
+
+NTHREADS=10
+nice -n 19 STAR --runThreadN $NTHREADS \
+     --genomeDir $GENOME/GRCh38_gencode_38 \
+     --readFilesCommand zcat \
+     --outFileNamePrefix $EXPERIMENT \
+     --readFilesIn $WD/pos_neg_r2.fastq.gz \
+     --outSAMattributes All \
+     --sjdbOverhang 61 \
+     --outSAMtype BAM SortedByCoordinate \
+     --sjdbGTFfile $GENOME/gencode.v38.basic.annotation.gtf \
+     --outFilterMismatchNmax 999 \
+     --seedSearchStartLmax 50
+
+# to get the information on the CIGAR
+
+samtools view $EXPERIMENT"Aligned.sortedByCoord.out.bam" | cut -f 1,3,6 > $EXPERIMENT"_CIGAR.txt"
