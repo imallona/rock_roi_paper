965d5e78f23c9fb6b090358474eed5a1f83ed2ee
Giulia Moro <giulia.moro2@uzh.ch>
2024-09-09 11:12:56 +0200

    new genome generation

--- 07_leukemia_patient/genome_set_up.sh
+++ 07_leukemia_patient/genome_set_up.sh
@@ -8,16 +8,13 @@ cd $WD
 human_fa=GRCh38.p13.genome.fa
 human_gtf=gencode.v38.basic.annotation.gtf
 
-captured_gtf=roi_bcr_abl.gtf
-
 wget http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/"$human_fa".gz
 wget http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/"$human_gtf".gz
 
 pigz --decompress *gz
 
-
-cat $human_gtf $captured_gtf > combined.gtf
-
 mv $human_fa combined.fa
 
-rm $human_gtf
+#rm $human_gtf
+
+mv $human_gtf combined.gtf
