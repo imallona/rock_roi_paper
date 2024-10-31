68fb92e68edfb8950942901d4400ccc08429f609
Giulia Moro <giulia.moro2@uzh.ch>
2024-10-31 13:10:48 +0100

    removed one of the exports

--- 08_leukemia_cell_lines/workflow/run.sh
+++ 08_leukemia_cell_lines/workflow/run.sh
@@ -7,11 +7,11 @@
 
 export PATH=/home/imallona/soft/star/STAR-2.7.10b/source:$PATH #path to STAR for STAR fusion
 export PATH=/home/gmoro/bwa/:$PATH #path to bwa
-export WD=~/data_mapping_leukemia_patient/workflow_aln #working directory
+export WD=~/cell_lines_leukemia/rock_roi_repo_for_cell_lines/workflow_aln #working directory
 export COMBINED_INDEXED_GENOME=~/mapping_leukemia/data/index # STAR indexed genome for STAR fusion
+export STARSOLO_BAM=/home/gmoro/cell_lines_leukemia/rock_roi_repo_for_cell_lines/output/align_tso/leukemia_cell_lines/Aligned.sortedByCoord.out.bam # TSO .bam file generated with rock_roi_method 
 export CUSTOM_FA=~/leukemia_bwamem2/genome/BCR_ABL.fa #file containing fusion cDNAs
 export NTHREADS=30 #number of threads
 export TRANSCRIPTOME=gencode.v38.pc_transcripts.fa #transcriptome to download
-export /home/gmoro/kiel_leukemia_data/mapping_patient_data/align_tso/leukemia_patient/Aligned.sortedByCoord.out.bam #path to .bam file, generated during script
 
 ./workflow.sh
