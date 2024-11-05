f1ab681ce08ed8ecee7c4ea2edff9de67548a47c
Giulia Moro <giulia.moro2@uzh.ch>
2024-11-05 09:15:38 +0100

    updated workflow

--- 07_leukemia_patient/workflow/run.sh
+++ 07_leukemia_patient/workflow/run.sh
@@ -13,5 +13,6 @@ export STARSOLO_BAM=/home/gmoro/kiel_leukemia_data/mapping_patient_data/align_ts
 export CUSTOM_FA=~/leukemia_bwamem2/genome/BCR_ABL.fa #file containing fusion cDNAs
 export NTHREADS=30 #number of threads
 export TRANSCRIPTOME=gencode.v38.pc_transcripts.fa #transcriptome to download
+export REGIONS=~/rock_roi_paper/07_leukemia_patient/simulation_patient_cell_lines/regions.bed
 
 ./workflow.sh
