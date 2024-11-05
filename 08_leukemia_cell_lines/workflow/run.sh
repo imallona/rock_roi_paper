6ed1545e894b6d42e7dc86bf2e705cb1132fa11b
Giulia Moro <giulia.moro2@uzh.ch>
2024-11-05 09:21:58 +0100

    added conda env activation

--- 08_leukemia_cell_lines/workflow/run.sh
+++ 08_leukemia_cell_lines/workflow/run.sh
@@ -15,4 +15,22 @@ export NTHREADS=30 #number of threads
 export TRANSCRIPTOME=gencode.v38.pc_transcripts.fa #transcriptome to download
 export REGIONS=~/rock_roi_paper/07_leukemia_patient/simulation_patient_cell_lines/regions.bed
 
+# installing conda and packages 
+
+#mkdir -p ~/miniconda3
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+#bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+
+source ~/miniconda3/bin/activate
+
+conda create --name fusion_detection
+conda activate fusion_detection
+
+conda config --add channels bioconda
+conda config --add channels conda-forge
+
+conda install -c conda-forge -c bioconda star=2.7.10b
+conda install -c conda-forge -c bioconda samtools=1.21 # v1.21
+conda install -c conda-forge -c bioconda bwa=0.7.18 #0.7.18
+
 ./workflow.sh
