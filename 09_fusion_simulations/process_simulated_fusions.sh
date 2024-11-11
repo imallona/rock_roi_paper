ff4bb2c361db1f912cdd3b3176dbf62b1f71b597
Izaskun Mallona <izaskun.mallona@gmail.com>
2024-11-11 13:28:23 +0100

    Add WTA and TSO capabilities

--- 09_fusion_simulations/process_simulated_fusions.sh
+++ 09_fusion_simulations/process_simulated_fusions.sh
@@ -38,17 +38,20 @@ echo 'Uncomment to install using conda/similar the environment env/fusion_conda_
 
 echo 'Extract UMIs/CBs'
 
-regex='^(?P<discard_1>.{0,3})(?P<cell_1>.{9})(?P<discard_2>GTGA)(?P<cell_2>.{9})(?P<discard_3>GACA)(?P<cell_3>.{9})(?P<umi_1>.{8}).*'
+#regex='^(?P<discard_1>.{0,3})(?P<cell_1>.{9})(?P<discard_2>GTGA)(?P<cell_2>.{9})(?P<discard_3>GACA)(?P<cell_3>.{9})(?P<umi_1>.{8}).*'
+regex='^(?P<discard_1>.{0,3})(?P<cell_1>.{9})(?P<cell_2>[GTGA|AATG]{4}){s<=1}(?P<cell_3>.{9})(?<cell_4>[GACA|CCAC]{4}){s<=1}(?P<cell_5>.{9})(?P<umi_1>.{8}).*'
 
-mkdir -p out
+
+mkdir -p out log
 
 ## so we label the cdnas with the CB and UMI
 umi_tools extract --extract-method=regex \
           --stdin=./data/fusion_simulations_cbumi.fq \
           --read2-in=./data/fusion_simulations_cdna.fq \
           --read2-out=./out/labelled_umis_cdna.fq.gz \
-          --bc-pattern="$regex" --log=processed.log --stdout out/processed.fastq.gz
+          --bc-pattern="$regex" --log=log/umitools.log --stdout out/processed.fastq.gz
 
+zcat ./out/labelled_umis_cdna.fq.gz | tail
 
 echo 'Scan for motifs (with mismatches, against a reference)'
 
@@ -56,24 +59,6 @@ zcat out/labelled_umis_cdna.fq.gz | \
     seqkit locate  \
            --max-mismatch 2 \
            --pattern-file data/reference_fusions.fa \
-           -j $NTHREADS > ./out/fusion_simulations_seqkit_locate.out
-       
-# This cannot be correct, the simulations are not ok because in line 3 we get a fused simulation aligning to
-## a nonfused reference?
-# seqID	patternName	pattern	strand	start	end	matched
-# major_13_fusion__overlaps_fusion=major__bcr=fused__abl=fused__expb=1__exps=1	BCRABL1_e14a2	TTTAAGCAGAGTTCAAAAGCCCTTCAGCGGCC 	+	76	108	TTTAAGCAGAGTTCAAAAGCCCTTCAGCGGCCA
-# major_13_fusion__overlaps_fusion=major__bcr=fused__abl=fused__expb=1__exps=1	wt_BCR_e13a2	ACCATCAATAAGGAAGATGATGAGTCTCCGGG	+	1	32	ACCATCAATAAGGAAGATGATGAGTCTCCGGG
+           -j $NTHREADS | gzip -c > ./out/fusion_locate_out.txt.gz
 
-
-
-# first, grep the reads with the fusion motifs
-## https://github.com/fulcrumgenomics/fqgrep
-
-## or perhaps with seqkit locate
-## https://bioinf.shenwei.me/seqkit/usage/#grep
-
-# then, collapse by umi seqkit rmdup?
-
-# rather umitools the fastqs, to add the UMI to each read name?
-# umi_tools dedup -I example.bam --output-stats=deduplicated -S deduplicated.bam
-
+## deduplicate by CB, UMI, pattern and start - here or in R?
