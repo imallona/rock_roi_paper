ad5d4b569bbe4368011f98d35daccc5125ed8fa0
Izaskun Mallona <izaskun.mallona@gmail.com>
2024-11-13 08:31:31 +0100

    Add new simulated truths from Giulia, but names don't match

--- 09_fusion_simulations/process_simulated_fusions.sh
+++ 09_fusion_simulations/process_simulated_fusions.sh
@@ -59,33 +59,28 @@ zcat ./out/labelled_umis_cdna.fq.gz | tail
 
 echo 'Scan for motifs (with mismatches, against a reference)'
 
-## without mismatches
-zcat out/labelled_umis_cdna.fq.gz | \
-    seqkit locate  \
-           --use-regexp \
-           --pattern-file data/reference_fusions.fa \
-           -j $NTHREADS | gzip -c > ./out/fusion_locate_out.txt.gz
-
-zcat ./out/fusion_locate_out.txt.gz | head
-
-## with mismatches
-
-zcat out/labelled_umis_cdna.fq.gz | \
-    seqkit locate  \
-           --max-mismatch 3 \
-           --pattern-file data/reference_fusions.fa \
-           -j $NTHREADS | gzip -c > ./out/fusion_locate_out_mm3.txt.gz
-
-zcat ./out/fusion_locate_out_mm3.txt.gz | head
-
-
+## five mm
 zcat out/labelled_umis_cdna.fq.gz | \
     seqkit locate  \
            --max-mismatch 5 \
            --pattern-file data/reference_fusions.fa \
            -j $NTHREADS | gzip -c > ./out/fusion_locate_out_mm5.txt.gz
 
-zcat ./out/fusion_locate_out_mm5.txt.gz | head
+zcat ./out/fusion_locate_out_mm5.txt.gz | head 
+
+
+# matched
+zcat out/fusion_locate_out_mm5.txt.gz | cut -f2 | grep -f - data/fusion_simulations_cdna.fq
+
+# missing
+zcat out/fusion_locate_out_mm5.txt.gz | cut -f2 | grep -v -f - data/fusion_simulations_cdna.fq | grep "^@"
+
+# ok, truths and simulated read names don't match still
+
+exit 123
+
+
+
 
 ## with regular expressions
 
@@ -99,3 +94,24 @@ zcat ./out/fusion_locate_out_reg.txt.gz | head
 
 
 ## deduplicate by CB, UMI, pattern and start - here or in R?
+
+
+# quick dirty check cell-lines
+giulia_cdna=/home/gmoro/test_leukemia_downsampled_cell_line_experiment/downsampled_R2_5M.fastq.gz
+giulia_cbumi=/home/gmoro/test_leukemia_downsampled_cell_line_experiment/downsampled_R1_5M.fastq.gz
+
+umi_tools extract --extract-method=regex \
+          --stdin="$giulia_cbumi" \
+          --read2-in="$giulia_cdna" \
+          --read2-out=./out/cellline_labelled_umis_cdna.fq.gz \
+          --bc-pattern="$regex" --log=log/cellline_umitools.log --stdout out/celllines.fq.gz
+
+
+pigz -dc -p 5 ./out/cellline_labelled_umis_cdna.fq.gz | \
+    seqkit locate  \
+           --use-regexp \
+           -G \
+           --pattern-file data/reference_fusions.fa \
+           -j 40 | gzip -c > ./out/cellline_regex_greedy.txt.gzip
+
+
